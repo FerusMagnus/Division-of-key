9ad7edebcc1712325bd8ae6f6a6911fbc0f7ca2f
FerusMagnus <38263282+FerusMagnus@users.noreply.github.com>
2018-04-13 02:23:37 +0300

    Воссоздал функцию фрагментирования ключа

---
+++ Division_of_key/Division_of_key/KeyDivisionSchemes.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Division_of_key
+{
+    class KeyDivisionSchemes
+    {
+        int _n;
+        int _p;
+        int[] _key;
+
+        public KeyDivisionSchemes(int n, int p, int[] key)
+        {
+            _n = n;
+            _p = p;
+            _key = key;
+        }
+
+        public int[,] FragmentationKey()
+        {
+            Random rand = new Random();
+
+            int[,] keyFragments = new int[_n, _key.Length];
+
+            for (int i = 0; i < _n - 1; i++)
+                for (int j = 0; j < _key.Length; j++)
+                    keyFragments[i, j] = Mod(rand.Next(), _p);
+
+            for (int j = 0; j < _key.Length; j++)
+            {
+                int buffer = _key[j];
+                for (int i = 0; i < _n - 1; i++)
+                    buffer -= keyFragments[i, j];
+
+                keyFragments[_n - 1, j] = Mod(buffer, _p);
+            }
+
+            return keyFragments;
+        }
+
+        public int Mod(double a, double b)
+        {
+            double q = Math.Floor(a / b);
+
+            return (int)(a - b * q);
+        }
+    }
+}
--- Division_of_key/Division_of_key/Program.cs
+++ Division_of_key/Division_of_key/Program.cs
@@ -10,6 +10,48 @@ namespace Division_of_key
     {
         static void Main(string[] args)
         {
+            bool flag = true;
+
+            Console.WriteLine("Добро пожаловать.");
+
+            Console.Write("Введите число сотрудников (n): ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите простое число (p): ");
+            int p = Convert.ToInt32(Console.ReadLine());           
+
+            Console.Write("Введите разделяемый ключ через пробел: ");
+            string[] input = Console.ReadLine().Split(new char[] { ' ', '\n', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+            int[] key = new int[input.Length];
+
+            for (int i = 0; i < key.Length; i++)
+                key[i] = Convert.ToInt32(input[i]);
+
+            KeyDivisionSchemes schema = new KeyDivisionSchemes(n, p, key);
+
+            // Фрагментируем ключ.
+            int[,] keyFragments = schema.FragmentationKey();
+
+            Console.WriteLine("Фрагменты разделяемого ключа:");
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                    Console.Write(keyFragments[i, j] + " ");
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Проверка:");
+            for (int j = 0; j < key.Length; j++)
+            {
+                int buffer = 0;
+                for (int i = 0; i < n; i++)
+                    buffer += keyFragments[i, j];
+
+                Console.Write(schema.Mod(buffer, p) + " ");
+            }
+
+            Console.ReadKey();
         }
     }
 }
